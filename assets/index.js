29c3daf2cacafb3a9fa33fe506546c6ce9ce295c
Christos Stefanis <xristostefanis@gmail.com>
2022-06-05 13:13:52 +0200

    create dark-light mode toggle

---
+++ assets/index.js
@@ -0,0 +1,81 @@
+import heartOutline from './heart-outline.png';
+import heart from './heart.png';
+import logo from './logo.png';
+import search from './Search.png';
+import tick from './tick.png';
+import cross from './cross.png';
+import logo02 from './logo02.png';
+import menu from './menu.png';
+import discord from './discord.png';
+import instagram from './instagram.png';
+import telegram from './telegram.png';
+import twitter from './twitter.png';
+import upload from './upload.png';
+import nft1 from './nft1.png';
+import nft2 from './nft2.png';
+import nft3 from './nft3.png';
+import nft4 from './nft4.png';
+import nft5 from './nft5.png';
+import nft6 from './nft6.jpeg';
+import nft7 from './nft7.png';
+import nft8 from './nft8.png';
+import nft9 from './nft9.png';
+import nft10 from './nft10.jpg';
+import creator from './creator.png';
+import creator1 from './creator1.png';
+import creator2 from './creator2.png';
+import creator3 from './creator3.png';
+import creator4 from './creator4.png';
+import creator5 from './creator5.png';
+import creator6 from './creator6.png';
+import creator7 from './creator7.png';
+import creator8 from './creator8.png';
+import creator9 from './creator9.png';
+import creator10 from './creator10.jpg';
+import right from './right.png';
+import left from './left.png';
+import headerbg from './headerbg.png';
+import loader from './loader.gif';
+import arrow from './arrow.png';
+
+export default {
+  heartOutline,
+  heart,
+  logo,
+  search,
+  tick,
+  cross,
+  logo02,
+  menu,
+  discord,
+  instagram,
+  telegram,
+  twitter,
+  upload,
+  nft1,
+  nft2,
+  nft3,
+  nft4,
+  nft5,
+  nft6,
+  nft7,
+  nft8,
+  nft9,
+  nft10,
+  creator,
+  creator1,
+  creator2,
+  creator3,
+  creator4,
+  creator5,
+  creator6,
+  creator7,
+  creator8,
+  creator9,
+  creator10,
+  right,
+  left,
+  headerbg,
+  loader,
+  arrow,
+};
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import images from '../assets';
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div className="flexCenter md:hidden cursor-pointer" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">PolyMarket</p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+          <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
