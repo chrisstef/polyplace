96b2a24c3e18333031a67eb25857b29b2791c613
Christos Stefanis <xristostefanis@gmail.com>
2022-06-28 14:15:12 +0200

    Replace head

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import { NFTContext } from '../context/NFTContext';
 import { Banner, CreatorCard, Loader, NFTCard, SearchBar } from '../components';
 
 import images from '../assets';
-import { makeId } from '../utils/makeId';
 import { getCreators } from '../utils/getTopCreators';
 import { shortenAddress } from '../utils/shortenAddress';
 
