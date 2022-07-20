91bc08f2ee43f122d24722ad2e328dc7134e6864
Christos Stefanis <xristostefanis@gmail.com>
2022-07-20 16:16:33 +0200

    head title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 
 import { NFTProvider } from '../context/NFTContext';
 import { Navbar, Footer } from '../components';
@@ -9,6 +10,10 @@ const MyApp = ({ Component, pageProps }) => (
   <NFTProvider>
     <ThemeProvider attribute="class">
       <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Head>
+          <title>Polyplace</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <Navbar />
         <div className="pt-65">
           <Component {...pageProps} />
