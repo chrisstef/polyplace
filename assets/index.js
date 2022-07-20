58f0c9005653d720dc484e7b4f64c701641678f9
Christos Stefanis <xristostefanis@gmail.com>
2022-07-20 16:00:31 +0200

    assets

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,5 @@
 import heartOutline from './heart-outline.png';
 import heart from './heart.png';
-import logo from './logo.png';
 import search from './Search.png';
 import tick from './tick.png';
 import cross from './cross.png';
@@ -11,17 +10,6 @@ import instagram from './instagram.png';
 import telegram from './telegram.png';
 import twitter from './twitter.png';
 import upload from './upload.png';
-import nft1 from './nft1.png';
-import nft2 from './nft2.png';
-import nft3 from './nft3.png';
-import nft4 from './nft4.png';
-import nft5 from './nft5.png';
-import nft6 from './nft6.jpeg';
-import nft7 from './nft7.png';
-import nft8 from './nft8.png';
-import nft9 from './nft9.png';
-import nft10 from './nft10.jpg';
-import creator from './creator.png';
 import creator1 from './creator1.png';
 import creator2 from './creator2.png';
 import creator3 from './creator3.png';
@@ -41,7 +29,6 @@ import arrow from './arrow.png';
 export default {
   heartOutline,
   heart,
-  logo,
   search,
   tick,
   cross,
@@ -52,17 +39,6 @@ export default {
   telegram,
   twitter,
   upload,
-  nft1,
-  nft2,
-  nft3,
-  nft4,
-  nft5,
-  nft6,
-  nft7,
-  nft8,
-  nft9,
-  nft10,
-  creator,
   creator1,
   creator2,
   creator3,
