d33d4519d631a69647e17d98bda1735df39b6eee
Christos Stefanis <xristostefanis@gmail.com>
2022-07-20 20:17:27 +0200

    transform button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ btnName, classStyles, handleClick }) => (
   <button
     type="button"
-    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
+    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white transform transition duration-500 hover:scale-105 ${classStyles}`}
     onClick={handleClick}
   >
     {btnName}
