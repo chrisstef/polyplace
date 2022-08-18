9533fa184d33955c15466e10839cd9fac696b35d
Christos Stefanis <xristostefanis@gmail.com>
2022-08-18 23:01:02 +0300

    upgraded context with new infura

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -12,11 +12,11 @@ const projectSecret = process.env.NEXT_PUBLIC_API_KEY_SECRET;
 
 const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString('base64')}`;
 const client = ipfsHttpClient({
-  host: 'polyplacedapp.infura-ipfs.io',
+  host: 'ipfs.infura.io',
   port: 5001,
   protocol: 'https',
   headers: {
-    authorization: auth,
+    auth,
   },
 });
 
