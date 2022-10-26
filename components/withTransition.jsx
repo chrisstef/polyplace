1543818fc3e43a7c85ac5f0afb72371388a52df0
Christos Stefanis <xristostefanis@gmail.com>
2022-10-26 23:02:10 +0200

    vercel analytics

--- components/withTransition.jsx
+++ components/withTransition.jsx
@@ -6,6 +6,9 @@ export default function withTransition(Component) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.3, ease: 'easeInOut' }}
+      // animate={{ width: '100%' }}
+      // exit={{ transition: { duration: 1 } }}
+
     >
       <Component {...props} />
     </motion.div>
