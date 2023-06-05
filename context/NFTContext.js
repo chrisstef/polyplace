4437fdc64e90f1ceafdd531381638798a44ae436
Christos Stefanis <xristostefanis@gmail.com>
2023-06-05 14:36:18 +0200

    restore website

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -1,245 +1,241 @@
-import React, { useEffect, useState } from "react";
-import Web3Modal from "web3modal";
-import { ethers } from "ethers";
-import axios from "axios";
-import { create as ipfsHttpClient } from "ipfs-http-client";
+import React, { useEffect, useState } from 'react';
+import Web3Modal from 'web3modal';
+import { ethers } from 'ethers';
+import axios from 'axios';
+import { create as ipfsHttpClient } from 'ipfs-http-client';
 
-import { MarketAddress, MarketAddressABI } from "./constants";
+import { MarketAddress, MarketAddressABI } from './constants';
 
-const subdomainName = "polyplace";
+const subdomainName = 'polyplace';
 
 const projectId = process.env.NEXT_PUBLIC_IPFS_PROJECT_ID;
 const projectSecret = process.env.NEXT_PUBLIC_API_KEY_SECRET;
 const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString(
-  "base64"
+    'base64',
 )}`;
 
 const endpointBasePath = `https://${subdomainName}.infura-ipfs.io/ipfs/`;
 
 const client = ipfsHttpClient({
-  host: "ipfs.infura.io",
-  port: 5001,
-  protocol: "https",
-  headers: {
-    authorization: auth,
-  },
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+        authorization: auth,
+    },
 });
 
-const fetchContract = (signerOrProvider) =>
-  new ethers.Contract(MarketAddress, MarketAddressABI, signerOrProvider);
+const fetchContract = (signerOrProvider) => new ethers.Contract(MarketAddress, MarketAddressABI, signerOrProvider);
 
 export const NFTContext = React.createContext();
 
 export const NFTProvider = ({ children }) => {
-  const nftCurrency = "MATIC";
-  const [currentAccount, setCurrentAccount] = useState("");
-  const [isLoadingNFT, setIsLoadingNFT] = useState(false);
+    const nftCurrency = 'MATIC';
+    const [currentAccount, setCurrentAccount] = useState('');
+    const [isLoadingNFT, setIsLoadingNFT] = useState(false);
 
-  const checkIfWalletIsConnect = async () => {
-    if (!window.ethereum) return alert("Please install MetaMask.");
+    const checkIfWalletIsConnect = async () => {
+        if (!window.ethereum) return alert('Please install MetaMask.');
 
-    const accounts = await window.ethereum.request({ method: "eth_accounts" });
-    if (accounts.length) {
-      setCurrentAccount(accounts[0]);
-    } else {
-      console.log("No accounts found");
-    }
-  };
+        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        if (accounts.length) {
+            setCurrentAccount(accounts[0]);
+        } else {
+            console.log('No accounts found');
+        }
+    };
 
-  useEffect(() => {
-    checkIfWalletIsConnect();
-  }, []);
+    useEffect(() => {
+        checkIfWalletIsConnect();
+    }, []);
 
-  const connectWallet = async () => {
-    if (!window.ethereum) return alert("Please install MetaMask.");
+    const connectWallet = async () => {
+        if (!window.ethereum) return alert('Please install MetaMask.');
 
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-
-    setCurrentAccount(accounts[0]);
-    window.location.reload();
-  };
-
-  const uploadToIPFS = async (file) => {
-    try {
-      const added = await client.add({ content: file });
-
-      const url = `${endpointBasePath}/${added.path}`;
-
-      console.log(`Upload to IPFS url: ${url}`);
-
-      return url;
-    } catch (error) {
-      console.log("error uploading file");
-    }
-  };
-
-  const createSale = async (url, formInputPrice, isReselling, id) => {
-    const web3Modal = new Web3Modal();
-    const connection = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(connection);
-    const signer = provider.getSigner();
-
-    const price = ethers.utils.parseUnits(formInputPrice, "ether");
-    const contract = fetchContract(signer);
-    const listingPrice = await contract.getListingPrice();
-
-    const transaction = !isReselling
-      ? await contract.createToken(url, price, {
-          value: listingPrice.toString(),
-        })
-      : await contract.resellToken(id, price, {
-          value: listingPrice.toString(),
+        const accounts = await window.ethereum.request({
+            method: 'eth_requestAccounts',
         });
 
-    setIsLoadingNFT(true);
-    await transaction.wait();
-  };
+        setCurrentAccount(accounts[0]);
+        window.location.reload();
+    };
 
-  const createNFT = async (formInput, fileUrl, router) => {
-    const { name, description, price } = formInput;
-
-    if (!name || !description || !price || !fileUrl) return;
-
-    const data = JSON.stringify({ name, description, image: fileUrl });
-
-    try {
-      const added = await client.add(data);
-      const url = endpointBasePath + added.path;
-
-      console.log(`Created NFT url: ${url}`);
-
-      await createSale(url, price);
-
-      router.push("/");
-    } catch (error) {
-      console.log("error uploading file");
-    }
-  };
-
-  const fetchNFTs = async () => {
-    setIsLoadingNFT(false);
-    const provider = new ethers.providers.AlchemyProvider(
-      "maticmum",
-      process.env.NEXT_PUBLIC_ALCHEMY_KEY
-    );
-    const contract = fetchContract(provider);
-    const data = await contract.fetchMarketItems();
-
-    const items = await Promise.all(
-      data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
-        const tokenURI = await contract.tokenURI(tokenId);
-        console.log("data" , tokenURI)
+    const uploadToIPFS = async (file) => {
         try {
-          const { data: { image, name, description } } = await axios.get(tokenURI);
-          const price = ethers.utils.formatUnits(unformattedPrice.toString(), "ether");
-          
-          // return an object with relevant properties
-          return {
-            price,
-            tokenId: tokenId.toNumber(),
-            seller,
-            owner,
-            image,
-            name,
-            description,
-            tokenURI,
-          };
+            const added = await client.add({ content: file });
+
+            const url = `${endpointBasePath}/${added.path}`;
+
+            console.log(`Upload to IPFS url: ${url}`);
+
+            return url;
         } catch (error) {
-          if (error.response && error.response.status === 404) {
-            // handle 404 error here
-            console.log("Token URI not found.");
-            return null;
-          } else {
-            // handle other errors here
-            console.error(error);
-            return null;
-          }
+            console.log('error uploading file');
         }
-        
-      })
-    );
+    };
 
-    return items;
-  };
+    const createSale = async (url, formInputPrice, isReselling, id) => {
+        const web3Modal = new Web3Modal();
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
 
-  const fetchMyNFTsOrListedNFTs = async (type) => {
-    setIsLoadingNFT(false);
-    const web3Modal = new Web3Modal();
-    const connection = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(connection);
-    const signer = provider.getSigner();
+        const price = ethers.utils.parseUnits(formInputPrice, 'ether');
+        const contract = fetchContract(signer);
+        const listingPrice = await contract.getListingPrice();
 
-    const contract = fetchContract(signer);
-    const data =
-      type === "fetchItemsListed"
-        ? await contract.fetchItemsListed()
-        : await contract.fetchMyNFTs();
+        const transaction = !isReselling
+            ? await contract.createToken(url, price, {
+                value: listingPrice.toString(),
+            })
+            : await contract.resellToken(id, price, {
+                value: listingPrice.toString(),
+            });
 
-    const items = await Promise.all(
-      data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
-        const tokenURI = await contract.tokenURI(tokenId);
-        const {
-          data: { image, name, description },
-        } = await axios.get(tokenURI);
-        const price = ethers.utils.formatUnits(
-          unformattedPrice.toString(),
-          "ether"
+        setIsLoadingNFT(true);
+        await transaction.wait();
+    };
+
+    const createNFT = async (formInput, fileUrl, router) => {
+        const { name, description, price } = formInput;
+
+        if (!name || !description || !price || !fileUrl) return;
+
+        const data = JSON.stringify({ name, description, image: fileUrl });
+
+        try {
+            const added = await client.add(data);
+            const url = endpointBasePath + added.path;
+
+            console.log(`Created NFT url: ${url}`);
+
+            await createSale(url, price);
+
+            router.push('/');
+        } catch (error) {
+            console.log('error uploading file');
+        }
+    };
+
+    const fetchNFTs = async () => {
+        setIsLoadingNFT(false);
+        const provider = new ethers.providers.AlchemyProvider(
+            'maticmum',
+            process.env.NEXT_PUBLIC_API_KEY,
+        );
+        const contract = fetchContract(provider);
+        const data = await contract.fetchMarketItems();
+
+        const items = await Promise.all(
+            data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
+                const tokenURI = await contract.tokenURI(tokenId);
+                console.log('data', tokenURI);
+                try {
+                    const { data: { image, name, description } } = await axios.get(tokenURI);
+                    const price = ethers.utils.formatUnits(unformattedPrice.toString(), 'ether');
+
+                    // return an object with relevant properties
+                    return {
+                        price,
+                        tokenId: tokenId.toNumber(),
+                        seller,
+                        owner,
+                        image,
+                        name,
+                        description,
+                        tokenURI,
+                    };
+                } catch (error) {
+                    if (error.response && error.response.status === 404) {
+                        // handle 404 error here
+                        console.log('Token URI not found.');
+                        return null;
+                    }
+                    // handle other errors here
+                    console.error(error);
+                    return null;
+                }
+            }),
         );
 
-        return {
-          price,
-          tokenId: tokenId.toNumber(),
-          seller,
-          owner,
-          image,
-          name,
-          description,
-          tokenURI,
-        };
-      })
+        return items;
+    };
+
+    const fetchMyNFTsOrListedNFTs = async (type) => {
+        setIsLoadingNFT(false);
+        const web3Modal = new Web3Modal();
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+
+        const contract = fetchContract(signer);
+        const data = type === 'fetchItemsListed'
+            ? await contract.fetchItemsListed()
+            : await contract.fetchMyNFTs();
+
+        const items = await Promise.all(
+            data.map(async ({ tokenId, seller, owner, price: unformattedPrice }) => {
+                const tokenURI = await contract.tokenURI(tokenId);
+                const {
+                    data: { image, name, description },
+                } = await axios.get(tokenURI);
+                const price = ethers.utils.formatUnits(
+                    unformattedPrice.toString(),
+                    'ether',
+                );
+
+                return {
+                    price,
+                    tokenId: tokenId.toNumber(),
+                    seller,
+                    owner,
+                    image,
+                    name,
+                    description,
+                    tokenURI,
+                };
+            }),
+        );
+        return items;
+    };
+
+    const buyNft = async (nft) => {
+        const web3Modal = new Web3Modal();
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+            MarketAddress,
+            MarketAddressABI,
+            signer,
+        );
+
+        const price = ethers.utils.parseUnits(nft.price.toString(), 'ether');
+        const transaction = await contract.createMarketSale(nft.tokenId, {
+            value: price,
+        });
+
+        setIsLoadingNFT(true);
+        await transaction.wait();
+        setIsLoadingNFT(false);
+    };
+
+    return (
+        <NFTContext.Provider
+            value={{
+                nftCurrency,
+                connectWallet,
+                currentAccount,
+                uploadToIPFS,
+                createNFT,
+                fetchNFTs,
+                fetchMyNFTsOrListedNFTs,
+                buyNft,
+                createSale,
+                isLoadingNFT,
+            }}
+        >
+            {children}
+        </NFTContext.Provider>
     );
-    return items;
-  };
-
-  const buyNft = async (nft) => {
-    const web3Modal = new Web3Modal();
-    const connection = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(connection);
-    const signer = provider.getSigner();
-    const contract = new ethers.Contract(
-      MarketAddress,
-      MarketAddressABI,
-      signer
-    );
-
-    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
-    const transaction = await contract.createMarketSale(nft.tokenId, {
-      value: price,
-    });
-
-    setIsLoadingNFT(true);
-    await transaction.wait();
-    setIsLoadingNFT(false);
-  };
-
-  return (
-    <NFTContext.Provider
-      value={{
-        nftCurrency,
-        connectWallet,
-        currentAccount,
-        uploadToIPFS,
-        createNFT,
-        fetchNFTs,
-        fetchMyNFTsOrListedNFTs,
-        buyNft,
-        createSale,
-        isLoadingNFT,
-      }}
-    >
-      {children}
-    </NFTContext.Provider>
-  );
 };
--- pages/index.js
+++ pages/index.js
@@ -11,172 +11,172 @@ import { getCreators } from '../utils/getTopCreators';
 import { shortenAddress } from '../utils/shortenAddress';
 
 const Home = () => {
-  const { fetchNFTs } = useContext(NFTContext);
-  const [hideButtons, setHideButtons] = useState(false);
-  const [nfts, setNfts] = useState([]);
-  const [nftsCopy, setNftsCopy] = useState([]);
-  const { theme } = useTheme();
-  const [activeSelect, setActiveSelect] = useState('Recently Added');
-  const [isLoading, setIsLoading] = useState(true);
+    const { fetchNFTs } = useContext(NFTContext);
+    const [hideButtons, setHideButtons] = useState(false);
+    const [nfts, setNfts] = useState([]);
+    const [nftsCopy, setNftsCopy] = useState([]);
+    const { theme } = useTheme();
+    const [activeSelect, setActiveSelect] = useState('Recently Added');
+    const [isLoading, setIsLoading] = useState(true);
 
-  const parentRef = useRef(null);
-  const scrollRef = useRef(null);
+    const parentRef = useRef(null);
+    const scrollRef = useRef(null);
 
-  useEffect(() => {
-    fetchNFTs()
-      .then((items) => {
-       const finalitems =  items.filter((v) => v !==null )
-        setNfts(finalitems);
-        setNftsCopy(finalitems);
-        setIsLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        fetchNFTs()
+            .then((items) => {
+                const finalitems = items.filter((v) => v !== null);
+                setNfts(finalitems);
+                setNftsCopy(finalitems);
+                setIsLoading(false);
+            });
+    }, []);
 
-  useEffect(() => {
-    const sortedNfts = [...nfts];
+    useEffect(() => {
+        const sortedNfts = [...nfts];
 
-    switch (activeSelect) {
-      case 'Price (low to high)':
-        setNfts(sortedNfts.sort((a, b) => a.price - b.price));
-        break;
-      case 'Price (high to low)':
-        setNfts(sortedNfts.sort((a, b) => b.price - a.price));
-        break;
-      case 'Recently Added':
-        setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
-        break;
-      default:
-        setNfts(nfts);
-        break;
-    }
-  }, [activeSelect]);
+        switch (activeSelect) {
+            case 'Price (low to high)':
+                setNfts(sortedNfts.sort((a, b) => a.price - b.price));
+                break;
+            case 'Price (high to low)':
+                setNfts(sortedNfts.sort((a, b) => b.price - a.price));
+                break;
+            case 'Recently Added':
+                setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
+                break;
+            default:
+                setNfts(nfts);
+                break;
+        }
+    }, [activeSelect]);
 
-  const onHandleSearch = (value) => {
-    const filteredNfts = nfts.filter(({ name }) => name.toLowerCase().includes(value.toLowerCase()));
+    const onHandleSearch = (value) => {
+        const filteredNfts = nfts.filter(({ name }) => name.toLowerCase().includes(value.toLowerCase()));
 
-    if (filteredNfts.length) {
-      setNfts(filteredNfts);
-    } else {
-      setNfts(nftsCopy);
-    }
-  };
-
-  const onClearSearch = () => {
-    if (nfts.length && nftsCopy.length) {
-      setNfts(nftsCopy);
-    }
-  };
-
-  const handleScroll = (direction) => {
-    const { current } = scrollRef;
-
-    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
-
-    if (direction === 'left') {
-      current.scrollLeft -= scrollAmount;
-    } else {
-      current.scrollLeft += scrollAmount;
-    }
-  };
-
-  const isScrollable = () => {
-    const { current } = scrollRef;
-    const { current: parent } = parentRef;
-
-    if (current?.scrollWidth >= parent?.offsetWidth) {
-      setHideButtons(false);
-    } else {
-      setHideButtons(true);
-    }
-  };
-
-  useEffect(() => {
-    isScrollable();
-
-    window.addEventListener('resize', isScrollable);
-
-    return () => {
-      window.removeEventListener('resize', isScrollable);
+        if (filteredNfts.length) {
+            setNfts(filteredNfts);
+        } else {
+            setNfts(nftsCopy);
+        }
     };
-  });
 
-  const topCreators = getCreators(nftsCopy);
+    const onClearSearch = () => {
+        if (nfts.length && nftsCopy.length) {
+            setNfts(nftsCopy);
+        }
+    };
 
-  return (
-    <div className="flex justify-center sm:px-4 p-12">
-      <div className="w-full minmd:w-4/5">
-        <Banner
-          name="We apologize for the inconvenience, but our website is currently undergoing maintenance."
-          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-center"
-          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
-        />
+    const handleScroll = (direction) => {
+        const { current } = scrollRef;
 
-        {!isLoading && !nfts.length ? (
-          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">The marketplace is empty.</h1>
-        ) : isLoading ? <Loader /> : (
-          <>
-            <div>
-              <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
-                ⭐ Top Creators
-              </h1>
-              <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
-                <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
-                  {topCreators.map((creator, i) => (
-                    <CreatorCard
-                      key={creator.seller}
-                      rank={i + 1}
-                      creatorImage={images[`creator${i + 1}`]}
-                      creatorName={shortenAddress(creator.seller)}
-                      creatorEths={creator.sum}
-                    />
-                  ))}
-                  {!hideButtons && (
-                  <>
-                    <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
-                      <Image
-                        src={images.left}
-                        layout="fill"
-                        objectFit="contain"
-                        alt="left_arrow"
-                        className={theme === 'light' ? 'filter invert' : ''}
-                      />
-                    </div>
-                    <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
-                      <Image
-                        src={images.right}
-                        layout="fill"
-                        objectFit="contain"
-                        alt="right_arrow"
-                        className={theme === 'light' ? 'filter invert' : ''}
-                      />
-                    </div>
-                  </>
-                  )}
-                </div>
-              </div>
+        const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+        if (direction === 'left') {
+            current.scrollLeft -= scrollAmount;
+        } else {
+            current.scrollLeft += scrollAmount;
+        }
+    };
+
+    const isScrollable = () => {
+        const { current } = scrollRef;
+        const { current: parent } = parentRef;
+
+        if (current?.scrollWidth >= parent?.offsetWidth) {
+            setHideButtons(false);
+        } else {
+            setHideButtons(true);
+        }
+    };
+
+    useEffect(() => {
+        isScrollable();
+
+        window.addEventListener('resize', isScrollable);
+
+        return () => {
+            window.removeEventListener('resize', isScrollable);
+        };
+    });
+
+    const topCreators = getCreators(nftsCopy);
+
+    return (
+        <div className="flex justify-center sm:px-4 p-12">
+            <div className="w-full minmd:w-4/5">
+                <Banner
+                    name="Own a piece of the digital revolution with unique NFT collectibles."
+                    childStyles="md:text-4xl sm:text-2xl xs:text-xl text-center"
+                    parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+                />
+
+                {!isLoading && !nfts.length ? (
+                    <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">The marketplace is empty.</h1>
+                ) : isLoading ? <Loader /> : (
+                    <>
+                        <div>
+                            <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
+                                ⭐ Top Creators
+                            </h1>
+                            <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+                                <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
+                                    {topCreators.map((creator, i) => (
+                                        <CreatorCard
+                                            key={creator.seller}
+                                            rank={i + 1}
+                                            creatorImage={images[`creator${i + 1}`]}
+                                            creatorName={shortenAddress(creator.seller)}
+                                            creatorEths={creator.sum}
+                                        />
+                                    ))}
+                                    {!hideButtons && (
+                                        <>
+                                            <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                                                <Image
+                                                    src={images.left}
+                                                    layout="fill"
+                                                    objectFit="contain"
+                                                    alt="left_arrow"
+                                                    className={theme === 'light' ? 'filter invert' : ''}
+                                                />
+                                            </div>
+                                            <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                                                <Image
+                                                    src={images.right}
+                                                    layout="fill"
+                                                    objectFit="contain"
+                                                    alt="right_arrow"
+                                                    className={theme === 'light' ? 'filter invert' : ''}
+                                                />
+                                            </div>
+                                        </>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="mt-10">
+                            <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+                                <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">🔥 Hot NFTs</h1>
+                                <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
+                                    <SearchBar
+                                        activeSelect={activeSelect}
+                                        setActiveSelect={setActiveSelect}
+                                        handleSearch={onHandleSearch}
+                                        clearSearch={onClearSearch}
+                                    />
+                                </div>
+                            </div>
+                            <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+                                {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+                            </div>
+                        </div>
+                    </>
+                )}
             </div>
-
-            <div className="mt-10">
-              <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
-                <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">🔥 Hot NFTs</h1>
-                <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
-                  <SearchBar
-                    activeSelect={activeSelect}
-                    setActiveSelect={setActiveSelect}
-                    handleSearch={onHandleSearch}
-                    clearSearch={onClearSearch}
-                  />
-                </div>
-              </div>
-              <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
-                {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default withTransition(Home);
