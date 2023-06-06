84c2ec3bedbcfd76d00b9cb297ebcae2503a6346
Christos Stefanis <xristostefanis@gmail.com>
2023-06-06 14:30:00 +0200

    create scroll to top button & functionality

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['polyplace.infura-ipfs.io'],
-  },
+    reactStrictMode: true,
+    images: {
+        domains: ['polyplace.infura-ipfs.io'],
+    },
 };
 
 module.exports = nextConfig;
