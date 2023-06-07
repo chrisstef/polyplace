e9a2ed169ab2314848b0df5df233401e1a688af4
Christos Stefanis <xristostefanis@gmail.com>
2023-06-07 15:08:21 +0200

    Dockerize the dapp (docker-compose)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 
 const nextConfig = {
     reactStrictMode: true,
+    webpackDevMiddleware: (config) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+        return config;
+    },
     images: {
         domains: ['polyplace.infura-ipfs.io'],
     },
