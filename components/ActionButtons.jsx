028c67138f14bcff7893030970878bbdfc18eab8
Christos Stefanis <xristostefanis@gmail.com>
2023-06-11 04:20:23 +0200

    Update ActionButtons UI

--- components/ActionButtons.jsx
+++ components/ActionButtons.jsx
@@ -1,40 +1,61 @@
-import React from 'react'
-import { BiRefresh, BiLinkExternal, BiCopy } from 'react-icons/bi'
-import { MdMoreVert } from 'react-icons/md'
+import React, { useState } from 'react';
+import { BiRefresh, BiLinkExternal, BiCopy } from 'react-icons/bi';
+import { MdMoreVert } from 'react-icons/md';
+
+import { MarketAddress } from '../context/constants';
 
 const ActionButtons = () => {
+    const [tooltipText, setTooltipText] = useState('');
+
+    const handleRefresh = () => {
+        window.location.reload();
+        setTooltipText('Refreshing...');
+    };
+
+    const handleExternalLink = () => {
+        const nftURL = `https://mumbai.polygonscan.com/token/${MarketAddress}?a=121`;
+        window.open(nftURL, '_blank');
+        setTooltipText('Redirecting...');
+    };
+
+    const handleCopyURL = () => {
+        const currentURL = window.location.href;
+        navigator.clipboard.writeText(currentURL);
+        setTooltipText('URL copied!');
+    };
+
+    const handleMouseLeave = () => {
+        setTooltipText(''); // Reset the tooltip text when the mouse leaves
+    };
 
     const actionItems = [
-        {
-            icon: <BiRefresh className='h-6 w-6 text-gray-500 dark:text-gray-400' />,
-            name: "Refresh"
-        },
-        {
-            icon: <BiLinkExternal className='h-6 w-6 text-gray-500 dark:text-gray-400' />,
-            name: "External"
-        },
-        {
-            icon: <BiCopy className='h-6 w-6 text-gray-500 dark:text-gray-400' />,
-            name: "Copy"
-        },
-        {
-            icon: <MdMoreVert className='h-6 w-6 text-gray-500 dark:text-gray-400' />,
-            name: "More"
-        }
-    ]
+        { icon: <BiRefresh onClick={handleRefresh} />, name: "Refresh" },
+        { icon: <BiLinkExternal onClick={handleExternalLink} />, name: "External" },
+        { icon: <BiCopy onClick={handleCopyURL} />, name: "Copy" },
+        { icon: <MdMoreVert />, name: "More" },
+    ];
+
+    const iconClassName = 'h-6 w-6 text-gray-500 dark:text-gray-400';
 
     return (
         <div className='relative flex divide-x divide-gray-300 rounded-lg border border-gray-300 shadow-sm cursor-pointer'>
-            {actionItems.map((item, index) => (
-                <div key={item.name} className='action-icon group'>
-                    {item.icon}
+            {actionItems.map((item) => (
+                <div
+                    key={item.name}
+                    className='action-icon group'
+                    onMouseLeave={handleMouseLeave}
+                >
+                    {React.cloneElement(item.icon, {
+                        className: iconClassName,
+                        onClick: item.icon.props.onClick,
+                    })}
                     <span className='action-tooltip group-hover:scale-100'>
-                        {item.name}
+                        {tooltipText || item.name}
                     </span>
                 </div>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default ActionButtons
+export default ActionButtons;
