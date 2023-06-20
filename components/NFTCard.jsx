de27b035da7ac0471d27778d0d9363502e9d7d57
Christos Stefanis <xristostefanis@gmail.com>
2023-06-20 13:16:05 +0200

    Manualy trigger redeploy

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -19,7 +19,7 @@ const NFTCard = ({ nft, onProfilePage }) => {
                 onHoverStart={e => { }}
                 onHoverEnd={e => { }}
                 whileInView={{ opacity: [0, 1] }}
-                transition={{ duration: 0.1 }}
+                transition={{ duration: 0.2 }}
                 className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md hover:shadow-lg duration-500"
             >
                 <div className="relative w-full h-52 sm:h-36 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
